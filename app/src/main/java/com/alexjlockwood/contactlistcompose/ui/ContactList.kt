0d2768c89cedb05acdc8aa26442ae9edc030c8d5
Alex Lockwood <alockwood@lyft.com>
2021-01-01 17:12:38 -0800

    cleanup lazy column

--- app/src/main/java/com/alexjlockwood/contactlistcompose/ui/ContactList.kt
+++ app/src/main/java/com/alexjlockwood/contactlistcompose/ui/ContactList.kt
@@ -1,11 +1,10 @@
 package com.alexjlockwood.contactlistcompose.ui
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumnForIndexed
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.remember
@@ -25,26 +24,24 @@ fun ContactList(
     // Remember a mutable state map of contact IDs to a boolean tracking
     // their card's current expanded state.
     val expandedContactsMap = remember { mutableStateMapOf<String, Boolean>() }
-    LazyColumnForIndexed(
-        items = contacts,
+    LazyColumn(
         modifier = modifier,
         contentPadding = contentPadding,
-    ) { index, contact ->
-        // Obtain this contact's current expanded state.
-        val isContactCardExpanded = expandedContactsMap[contact.id] ?: false
-        ContactCard(
-            contact = contact,
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable(onClick = {
-                    // Toggle the card's expanded state each time it is clicked.
-                    expandedContactsMap[contact.id] = !isContactCardExpanded
-                }),
-            isExpanded = isContactCardExpanded,
-        )
-        if (index < contacts.size - 1) {
-            // Add a bottom margin beneath every card except the last.
-            Spacer(modifier = Modifier.height(16.dp))
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+        items(contacts) { contact ->
+            // Obtain this contact's current expanded state.
+            val isContactCardExpanded = expandedContactsMap[contact.id] ?: false
+            ContactCard(
+                contact = contact,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable(onClick = {
+                        // Toggle the card's expanded state each time it is clicked.
+                        expandedContactsMap[contact.id] = !isContactCardExpanded
+                    }),
+                isExpanded = isContactCardExpanded,
+            )
         }
     }
 }
